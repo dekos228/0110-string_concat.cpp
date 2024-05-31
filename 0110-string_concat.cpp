6745883dbdb9fdbbafea6c4ccf165ebe5e17b8ca
dekos228 <dekos589191@gmail.com>
2024-05-31 20:27:54 +0200

    Create 0110-string_concat.cpp

---
+++ 0110-string_concat.cpp
@@ -0,0 +1,16 @@
+﻿// string_concat.cpp
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+int main()
+{
+  using namespace std;
+
+  string line1, line2; // Сразу две переменные, пустые строки.
+  getline(cin, line1);
+  getline(cin, line2);
+
+  cout << (line1 + line2) << endl; // + "склеивает" строки.
+  return EXIT_SUCCESS;
+}
